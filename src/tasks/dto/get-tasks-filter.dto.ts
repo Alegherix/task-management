4727a72c07b7ffe085419ece33ff831e3056bfac
Alegherix <psao69@gmail.com>
2020-11-05 09:16:57 +0100

    Added typeorm and created entity to represent a task

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsIn(Object.values(TaskStatus))
+  @IsOptional()
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
