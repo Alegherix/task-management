99286d64497c582938cddcc726be9c1d648fed9d
Alegherix <psao69@gmail.com>
2020-11-09 18:33:31 +0100

    Added hashing of password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @MinLength(5)
+  @MaxLength(20)
+  @IsString()
   username: string;
+
+  @MinLength(8)
+  @MaxLength(20)
+  @IsString()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain atleast \n 1 Upperchase letter\n 1 Lowercase letter\n 1 Number or special character and be between 8-20 characters.',
+  })
   password: string;
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,8 @@
+import * as argon2 from 'argon2';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -7,10 +12,22 @@ export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const hashedPassword = await argon2.hash(password);
+
     const user = new User();
     user.username = username;
-    // Ändra detta senare till att calla argons hashing method.
-    user.pasword = password;
-    await user.save();
+    user.pasword = hashedPassword;
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        // Duplicate Username
+        throw new ConflictException('Username already exists');
+      } else {
+        console.log();
+
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
