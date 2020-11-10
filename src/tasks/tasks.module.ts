b50571ebc0ec2e3ede34e6e031c657fa71189def
Alegherix <psao69@gmail.com>
2020-11-10 21:14:08 +0100

    added AuthGuards and auth req for tasks endpoints

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 import { TaskRepository } from './task.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
@@ -7,7 +8,7 @@ import { TasksService } from './tasks.service';
 @Module({
   // Detta gör att typeOrmModulen, importerar TaskRepository via DI, och gör den tillgänglig i hela denna modulen
   // Lite som att passera en resolver till buildSchema.
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [TypeOrmModule.forFeature([TaskRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
