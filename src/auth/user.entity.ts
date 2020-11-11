49e4580259c939d8577a4a5a07e56b8e5d11a0c1
Alegherix <psao69@gmail.com>
2020-11-11 17:31:28 +0100

    Created OneToMany of tasks for user and reverse

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -2,10 +2,12 @@ import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
 import * as argon2 from 'argon2';
+import { Task } from '../tasks/task.entity';
 
 @Entity()
 @Unique(['username'])
@@ -18,6 +20,14 @@ export class User extends BaseEntity {
 
   @Column()
   pasword: string;
+
+  // Sätter upp one to many
+  @OneToMany(
+    type => Task,
+    task => task.user,
+    { eager: true },
+  )
+  tasks: Task[];
 
   async validatePassword(password: string): Promise<boolean> {
     return await argon2.verify(this.pasword, password);
